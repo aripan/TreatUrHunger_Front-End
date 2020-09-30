5e7c2484db048d0417b8054c59278fe28e6591c6
aripan <aripan017@gmail.com>
2020-09-30 23:33:20 +0200

    updated baseUrl

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,5 +1,5 @@
-export const baseUrl = "https://localhost:5443/";
+// export const baseUrl = "https://localhost:5443/";
 
 // export const baseUrl = "http://localhost:3001/";
 
-// export const baseUrl = "https://treaturhungerserver.herokuapp.com/";
+export const baseUrl = "https://treaturhungerserver.herokuapp.com/";
