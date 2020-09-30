d45040175d249b6c52914afec8d18465ca10f707
aripan <aripan017@gmail.com>
2020-09-30 16:08:14 +0200

    Path modified for homePage

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -213,6 +213,7 @@ class Main extends Component {
         <div>
           <Switch>
             <Route
+              exact
               path="/register"
               component={() => (
                 <Register
@@ -254,7 +255,7 @@ class Main extends Component {
                 />
               )}
             />
-            <Redirect to="/home" />
+            <Redirect to="/" />
           </Switch>
         </div>
 
